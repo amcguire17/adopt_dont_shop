99f950ecde4a89dc7c0eef2ecf2c2e5cc3fcbd4c
Amanda McGuire <aruwart001@gmail.com>
2021-07-18 16:44:44 -0600

    Add search for pets

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,6 +4,7 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
+    @pets = Pet.search(params[:search])
   end
 
   def new
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -8,5 +8,34 @@ RSpec.describe 'Application Show Page' do
     @app = Application.create!(applicant_name: 'Freddy', applicant_street_address: '13 Walk Way', applicant_city: 'Aurora', applicant_state: 'CO', applicant_zip_code: '82012', status: 'In Progress')
   end
   it 'shows an application and all attributes' do
+  it 'can search for a pet' do
+    visit "/applications/#{@app.id}"
+
+    fill_in :search, with: 'Lobster'
+    click_button 'Search'
+
+    expect(current_path).to eq("/applications/#{@app.id}")
+    expect(page).to have_content('Lobster')
+
+  end
+
+  it 'can search for animal by partial matches' do
+    visit "/applications/#{@app.id}"
+
+    fill_in :search, with: 'Lob'
+    click_button 'Search'
+
+    expect(current_path).to eq("/applications/#{@app.id}")
+    expect(page).to have_content('Lobster')
+  end
+
+  it 'can search for animal by case insensitive' do
+    visit "/applications/#{@app.id}"
+
+    fill_in :search, with: 'lucille'
+    click_button 'Search'
+
+    expect(current_path).to eq("/applications/#{@app.id}")
+    expect(page).to have_content('Lucille Bald')
   end
 end
